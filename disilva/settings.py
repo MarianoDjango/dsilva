7a7e973ea18a1d75cb0e381c8dc5367c08735f0c
MarianoDjango <mscagliarat@yahoo.com.ar>
2026-06-14 20:56:49 -0300

    settings pequeño error

--- disilva/settings.py
+++ disilva/settings.py
@@ -104,7 +104,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
     'myapp',
     'widget_tweaks',
     'cloudinary',
